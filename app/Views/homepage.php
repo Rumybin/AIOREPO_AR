89d7449ef75ca54fd8c0da763a78ed9c63c6ffc7
Rumybin <pelajarbintangrumy@gmail.com>
2024-10-04 22:20:25 +0700

    add new home update slideshow #1

--- app/Views/homepage.php
+++ app/Views/homepage.php
@@ -14,17 +14,49 @@
     <div class="main-content">
         <h2>INI EKSTRA</h2>
 
-        <a href="/olim">
-            <div class="box large-box">
-                <img src="/images/olim.png" alt="Olimpiad Image">
-                <div class="overlay-text">OLYMPIAD</div>
-            </div>
-        </a>
+         <!-- Slideshow section -->
+        <div class="slideshow-container">
+                <div class="mySlides fade">
+                    <img src="<?= base_url('images/ichi.png'); ?>" alt="Slide 1">
+                </div>
 
-        <div class="small-box-container">
-            <div class="box small-box">INI 1</div>
-            <div class="box small-box">INI 2</div>
+                <div class="mySlides fade">
+                    <img src="<?= base_url('images/ni.png'); ?>" alt="Slide 2">
+                </div>
+
+                <div class="mySlides fade">
+                    <img src="<?= base_url('images/san.png'); ?>" alt="Slide 3">
+                </div>
+            </div>
+
+            <!-- Dot indicators -->
+            <div style="text-align:center">
+                <span class="dot" onclick="currentSlide(1)"></span>
+                <span class="dot" onclick="currentSlide(2)"></span>
+                <span class="dot" onclick="currentSlide(3)"></span>
+            </div>
+
+        <!-- Container for slideshow and boxes -->
+        <div class="container">
+           
+
+            <!-- Large box -->
+            <a href="/olim">
+                <div class="box large-box">
+                    <img src="/images/olim.png" alt="Olimpiad Image">
+                    <div class="overlay-text">OLYMPIAD</div>
+                </div>
+            </a>
+
+            <!-- Small boxes -->
+            <div class="small-box-container">
+                <div class="box small-box">INI 1</div>
+                <div class="box small-box">INI 2</div>
+            </div>
         </div>
     </div>
+
+    <!-- Memuat file JS -->
+    <script src="<?= base_url('js/slideshow.js'); ?>"></script>
 </body>
 </html>
